c40fcde91c771ac82b9bc86c0a9a80fea632316d
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2024-12-04 08:36:46 +0100

    Update setup.js

--- www/js/setup.js
+++ www/js/setup.js
@@ -1,4 +1,4 @@
-// Game Setup
+
 import { API_URL } from './config/config.js';
 
 const { API } = await import('./classes/API.js');
@@ -9,12 +9,11 @@ const api = new API(API_URL);
 const game = new Game(api);
 
 
-// Event listener for game actions (Hit, Stand, etc.)
+
 document.querySelector('.hit-button').addEventListener('click', () => {
     game.hit()
         .then(response => {
             console.log('Card dealt:', response);
-            // Update game state here (update UI with new cards)
         })
         .catch(error => {
             console.error('Hit action error:', error);
@@ -25,7 +24,6 @@ document.querySelector('.stand-button').addEventListener('click', () => {
     game.stand()
         .then(response => {
             console.log('Player stands:', response);
-            // Update game state here (end player's turn)
         })
         .catch(error => {
             console.error('Stand action error:', error);
